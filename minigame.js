79537058f3def62c64adf892a439e35e5231b816
GwijaCodes <133336462+GwijaCodes@users.noreply.github.com>
2023-11-06 19:57:36 +0100

    Add files via upload

--- minigame.js
+++ minigame.js
@@ -1,8 +1,7 @@
 const player = document.querySelector('.player');
 const block = document.querySelector('.block');
+const skills = document.querySelector('#skills')
 const board = document.querySelector('.board');
-const progettiText = document.querySelector('#playGame');
-const score = document.querySelector('#score');
 const rat = document.querySelector('#rat');
 
 board.addEventListener('click', jump);
@@ -18,30 +17,53 @@ function jump(){
     }
 }
 
+function ouch(){
+      rat.style.filter = 'sepia(66%) brightness(77%) hue-rotate(304deg) saturate(319%) contrast(182%)'
+     
+    setTimeout(function(){
+        rat.style.filter = 'none'
+    }, 600)
+}
+
 const checkDeath = setInterval(function(){
     const playerTop = parseInt(window.getComputedStyle(player).getPropertyValue('top'));
     const blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue('left'));
     if(blockLeft < 30 && blockLeft > 0 && playerTop >= 70){
         block.classList.remove('block-move')
-        alert('Whoops!')
+        jump();
+        ouch();
     }
 }, 10);
 
+
+let blockSkins = [
+    './imgs/html.svg',
+    './imgs/css.svg',
+    './imgs/js.svg',
+    './imgs/blender.svg',
+    './imgs/adobe.svg',
+]
+
+let skinIndex = 0;
+
+block.addEventListener('animationiteration', changeSkin, false)
+
+function changeSkin(){
+    skills.src = blockSkins[skinIndex];
+    skinIndex >= 4 ? skinIndex = 0 : skinIndex++
+}
 
 function gameOn(){
     if(block.classList != 'block-move'){
         block.classList.add('block-move');
         block.style.animationPlayState = 'running';
-        progettiText.innerHTML = '&#9654;';
-        progettiText.style.transform = 'translateY(5px)'
     }
+}
+
+window.onload = function(){
 }
 
 function gameOff(){
          block.style.animationPlayState = 'paused';
-         progettiText.innerHTML = "&#10074;&#10074;";
-         progettiText.style.color = '#D9D9D9';
-        progettiText.style.transform = 'translateY(-1px)'
-
 }
 
